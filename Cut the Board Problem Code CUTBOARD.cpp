51d1fbc9ebfb02e49d8d13fb896f534185af8de9
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2021-04-01 00:23:04 +0600

    Create Cut the Board Problem Code CUTBOARD.cpp

---
+++ Cut the Board Problem Code CUTBOARD.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int T ;
+    cin >> T ;
+
+    int r , c ;
+
+    while (T--)
+    {
+        cin >> r >> c ;
+
+        cout << (r -1 ) * ( c -1 ) <<endl;;
+
+    }
+
+    return 0 ;
+
+
+}
